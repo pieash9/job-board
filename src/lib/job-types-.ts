bd3a66d5aca19a4282cbd8730ff82dfd1688af89
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-05-09 22:19:49 +0600

    complete server action at job filter sidebar

---
+++ src/lib/job-types-.ts
@@ -0,0 +1,10 @@
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Internship",
+  "Temporary",
+  "Volunteer",
+];
+
+export const locationTypes = ["Remote", "On-site", "Hybrid"];
