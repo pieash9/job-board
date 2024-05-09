42fa8617a216cde23c716c24842acbfbe67618e6
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-05-09 22:59:55 +0600

    added search job functionality

---
+++ src/components/JobResults.tsx
@@ -0,0 +1,59 @@
+import prisma from "@/lib/prisma";
+import JobListItem from "./JobListItem";
+import { JobFilterValues } from "@/lib/validation";
+import { Prisma } from "@prisma/client";
+
+interface JobResultsProps {
+  filterValues: JobFilterValues;
+}
+
+const JobResults = async ({
+  filterValues: { q, type, location, remote },
+}: JobResultsProps) => {
+  const searchString = q
+    ?.split(" ")
+    .filter((word) => word.length > 0)
+    .join(" & ");
+
+  const searchFilter: Prisma.JobWhereInput = searchString
+    ? {
+        OR: [
+          { title: { search: searchString, mode: "insensitive" } },
+          { companyName: { search: searchString, mode: "insensitive" } },
+          { type: { search: searchString, mode: "insensitive" } },
+          { location: { search: searchString, mode: "insensitive" } },
+          { locationType: { search: searchString, mode: "insensitive" } },
+        ],
+      }
+    : {};
+
+  const where: Prisma.JobWhereInput = {
+    AND: [
+      searchFilter,
+      type ? { type } : {},
+      location ? { location } : {},
+      remote ? { locationType: "Remote" } : {},
+      { approved: true },
+    ],
+  };
+
+  const jobs = await prisma.job.findMany({
+    where,
+    orderBy: { createdAt: "desc" },
+  });
+  return (
+    <div className="grow space-y-4">
+      {jobs.map((job) => (
+        <JobListItem key={job.slug} job={job} />
+      ))}
+
+      {jobs.length === 0 && (
+        <p className="m-auto text-center text-muted-foreground">
+          No jobs found! Try adjusting your search filters.
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default JobResults;
---
+++ src/components/ui/h1.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+
+const H1 = ({ className, ...props }: React.HTMLProps<HTMLHeadingElement>) => {
+  return (
+    <h1
+      className={cn(
+        "text-4xl font-extrabold tracking-tight lg:text-5xl",
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+export default H1;
